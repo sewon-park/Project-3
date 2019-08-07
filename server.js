315bcc055fd4d649dfb4509e012a2d8e3fe3f4bf
won518 <sewonpark518@gmail.com>
2019-08-07 09:08:33 -0700

    removing env from port

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const routes = require("./routes");
 // Require cookie packages
 var cookieParser = require("cookie-parser");
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.PORT || 5000;
 const app = express();
 
 // Define middleware here
